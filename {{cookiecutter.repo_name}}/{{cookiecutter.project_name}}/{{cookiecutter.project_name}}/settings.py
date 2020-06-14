efe859cced5743fb1448870db57b46c643cdf88a
Aftab Hussain <py-aftabhussain@mobiloitte.com>
2020-06-14 11:37:13 +0530

    initial commit

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -48,19 +48,19 @@ CUSTOM_APPS = [
 ]
 
 THIRD_PARTY = [
-    { % if cookiecutter.api == "y" or cookiecutter.api == "Y" % }
+    {% if cookiecutter.api == "y" or cookiecutter.api == "Y" %}
     'rest_framework',
     'drf_yasg',
     'corsheaders',
-    { % endif % }
+    {% endif %}
 ]
 
 INSTALLED_APPS += CUSTOM_APPS + THIRD_PARTY
-{ % if cookiecutter.api == "y" or cookiecutter.api == "Y" % }
+{% if cookiecutter.api == "y" or cookiecutter.api == "Y" %}
 
 if DEBUG is True:
     CORS_ORIGIN_ALLOW_ALL = True
-{ % endif % }
+{% endif %}
 
 
 CORS_ORIGIN_WHITELIST = [
@@ -144,7 +144,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-{ % if cookiecutter.api == "y" or cookiecutter.api == "Y" % }
+{% if cookiecutter.api == "y" or cookiecutter.api == "Y" %}
 # Django Rest Framework
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
@@ -197,7 +197,7 @@ SWAGGER_SETTINGS = {
         },
     },
 }
-{ % endif % }
+{% endif %}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
