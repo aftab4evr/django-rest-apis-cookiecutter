3a8a22c7d52ae9883190f2204e74dfe9de11372f
Aftab Hussain <py-aftabhussain@mobiloitte.com>
2020-06-14 12:23:32 +0530

    initial commit

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic.base import TemplateView
 
-{% if cookiecutter.api == "y" or cookiecutter.api == "Y" % }
+{% if cookiecutter.api == "y" or cookiecutter.api == "Y" %}
 schema_view = get_schema_view(
     openapi.Info(
         title="{{ cookiecutter.project_name }} API",
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
     public=False,
     permission_classes=(permissions.AllowAny,),
 )
-{% endif % }
+{% endif %}
 
 admin.site.site_title = "{{ cookiecutter.project_name|title }} Administration"
 admin.site.site_header = "{{ cookiecutter.project_name|title }} Administration"
@@ -34,7 +34,7 @@ handler404 = '{{ cookiecutter.project_name }}.views.handler404'
 handler500 = '{{ cookiecutter.project_name }}.views.handler500'
 
 if settings.DEBUG:
-    {% if cookiecutter.api == "y" or cookiecutter.api == "Y" % }
+    {% if cookiecutter.api == "y" or cookiecutter.api == "Y" %}
     urlpatterns += [
         re_path(r'^document(?P<format>\.json|\.yaml)$',
                 schema_view.without_ui(cache_timeout=0), name='schema-json'),
@@ -45,7 +45,7 @@ if settings.DEBUG:
         path('api-auth/', include('rest_framework.urls')),
         # include('auth.urls', namespace='auth')),
     ]
-    {% endif % }
+    {% endif %}
     urlpatterns += [
         # Testing 404 and 500 error pages
         path('404/', TemplateView.as_view(template_name='404.html'), name='404'),
